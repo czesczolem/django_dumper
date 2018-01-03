fa6a6b3685b6d6c91c85e71d01539c8bea1a76a8
Maciej Tatarek <mtatarek@opera.com>
2018-01-03 11:45:39 +0100

    Add Dump class

--- dumper/models.py
+++ dumper/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Dump(models.model):
+    filename = models.CharField(max_length=100, blank=False)
+    date = models.DateTimeField()
+
+    def __str__(self):
+        return self.filename
